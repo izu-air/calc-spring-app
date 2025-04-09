3677e8bc9d95bf799c0cb126a1484309ee9de66a
izu-air <77841658+izu-air@users.noreply.github.com>
2025-04-09 18:23:24 +0300

    Update PracticeApplicationTests.java

--- WorkPractice2025/src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
+++ WorkPractice2025/src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
@@ -1,13 +1,47 @@
 package ru.neoflex.practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PracticeApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testAdditionOfTwoPositiveVariables() throws Exception {
+		mockMvc.perform(get("/plus/5/10"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("15"));
 	}
 
+	@Test
+	void testAdditionOfTwoNegativeVariables() throws Exception {
+		mockMvc.perform(get("/plus/-5/-10"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("-15"));
+	}
+
+	@Test
+	void testSubtractingOfTwoPositiveVariables() throws Exception {
+		mockMvc.perform(get("/minus/10/5"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("5"));
+	}
+
+	@Test
+	void testSubtractingOfTwoNegativeVariables() throws Exception {
+		mockMvc.perform(get("/minus/-10/-5"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("-5"));
+	}
 }
+
